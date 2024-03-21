35d8227124c735b483297493e1528146698d9510
ichanddan <code.us.cm@gmail.com>
2024-03-21 22:33:59 +0530

    Add Team Section and Update some deta

--- src/components/About.js
+++ src/components/About.js
@@ -4,47 +4,48 @@ import { Link } from "react-router-dom";
 
 export default function About() {
   return (
-    <div class="pt-10 overflow-hidden md:pt-0 sm:pt-16 2xl:pt-16">
-      <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
-        <div class="grid items-center grid-cols-1 md:grid-cols-2">
+    <div className="pt-10 overflow-hidden md:pt-0 sm:pt-16 2xl:pt-16">
+      <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+        <div className="grid items-center grid-cols-1 md:grid-cols-2">
           <div>
-            <h2 class="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
+            <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
               Hey 👋 I am
-              <br class="block sm:hidden" />
+              <br className="block sm:hidden" />
               Chandan
             </h2>
-            <p class="max-w-lg mt-3 text-xl leading-relaxed text-gray-600 md:mt-8">
+            <p className="max-w-lg mt-3 text-xl leading-relaxed text-gray-600 md:mt-8">
               Amet minim mollit non deserunt ullamco est sit aliqua dolor do
               amet sint. Velit officia consequat duis enim velit mollit.
               Exercitation veniam consequat sunt nostrud amet.
             </p>
 
-            <p class="mt-4 text-xl text-gray-600 md:mt-8">
-              <span class="relative inline-block">
-                <span class="absolute inline-block w-full bottom-0.5 h-2 bg-yellow-300"></span>
-                <span class="relative"> Have a question? </span>
+            <p className="mt-4 text-xl text-gray-600 md:mt-8">
+              <span className="relative inline-block">
+                <span className="absolute inline-block w-full bottom-0.5 h-2 bg-yellow-300"></span>
+                <span className="relative"> Have a question? </span>
               </span>
-              <br class="block sm:hidden" />
+              <br className="block sm:hidden" />
               Ask me on{" "}
               <Link
                 to="https://linkedin.com/in/ichanddan"
                 title=""
-                class="transition-all duration-200 text-sky-500 hover:text-sky-600 hover:underline"
+                target="_blank"
+                className="transition-all duration-200 text-sky-500 hover:text-sky-600 hover:underline"
               >
                 Linkedin
               </Link>
             </p>
           </div>
 
-          <div class="relative">
-            <img
-              class="absolute inset-x-0 bottom-0 -mb-48 -translate-x-1/2 left-1/2"
-              src="https://cdn.rareblocks.xyz/collection/celebration/images/team/1/blob-shape.svg"
+          <div className="relative">
+            <Image
+              className="absolute inset-x-0 bottom-0 -mb-48 -translate-x-1/2 left-1/2"
+              src="/blob-shape.png"
               alt=""
             />
 
-            <img
-              class="relative w-full xl:max-w-lg xl:mx-auto 2xl:origin-bottom 2xl:scale-110"
+            <Image
+              className="relative w-full xl:max-w-lg xl:mx-auto 2xl:origin-bottom 2xl:scale-110"
               src="https://cdn.rareblocks.xyz/collection/celebration/images/team/1/business-woman.png"
               alt=""
             />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,181 +1,17 @@
-import { Image } from "@nextui-org/react";
 import { Accordion, AccordionItem } from "@nextui-org/react";
 import React from "react";
+import Team from "./Home Deta/Team";
+import Leader from "./Home Deta/Leader";
+import Productivity from "./Home Deta/Productivity";
 
 export default function Home() {
   const defaultContent =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
   return (
     <div>
-      {/* <!-- Section 3 --> */}
-      <section className="w-full bg-white pt-7 pb-7 md:pt-20 md:pb-24">
-        <div className="box-border flex flex-col items-center content-center px-8 mx-auto leading-6 text-black border-0 border-gray-300 border-solid md:flex-row max-w-7xl lg:px-16">
-          {/* <!-- Image --> */}
-          <div className="box-border relative w-full max-w-md px-4 mt-5 mb-4 -ml-5 text-center bg-no-repeat bg-contain border-solid md:ml-0 md:mt-0 md:max-w-none lg:mb-0 md:w-1/2 xl:pl-10">
-            <Image
-              src="https://cdn.devdojo.com/images/december2020/productivity.png"
-              className="p-2 pl-6 pr-5 xl:pl-16 xl:pr-20 "
-            />
-          </div>
-
-          {/* <!-- Content --> */}
-          <div className="box-border order-first w-full text-black border-solid md:w-1/2 md:pl-10 md:order-none">
-            <h2 className="m-0 text-xl font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl">
-              Boost Productivity
-            </h2>
-            <p className="pt-4 pb-8 m-0 leading-7 text-gray-700 border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-lg">
-              Build an atmosphere that creates productivity in your organization
-              and your company culture.
-            </p>
-            <ul className="p-0 m-0 leading-6 border-0 border-gray-300">
-              <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
-                <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
-                  <span className="text-sm font-bold">✓</span>
-                </span>{" "}
-                Maximize productivity and growth
-              </li>
-              <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
-                <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
-                  <span className="text-sm font-bold">✓</span>
-                </span>{" "}
-                Speed past your competition
-              </li>
-              <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
-                <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
-                  <span className="text-sm font-bold">✓</span>
-                </span>{" "}
-                Learn the top techniques
-              </li>
-            </ul>
-          </div>
-          {/* <!-- End  Content --> */}
-        </div>
-        <div className="box-border flex flex-col items-center content-center px-8 mx-auto mt-2 leading-6 text-black border-0 border-gray-300 border-solid md:mt-20 xl:mt-0 md:flex-row max-w-7xl lg:px-16">
-          {/* <!-- Content --> */}
-          <div className="box-border w-full text-black border-solid md:w-1/2 md:pl-6 xl:pl-32">
-            <h2 className="m-0 text-xl font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl">
-              Automated Tasks
-            </h2>
-            <p className="pt-4 pb-8 m-0 leading-7 text-gray-700 border-0 border-gray-300 sm:pr-10 lg:text-lg">
-              Save time and money with our revolutionary services. We are the
-              leaders in the industry.
-            </p>
-            <ul className="p-0 m-0 leading-6 border-0 border-gray-300">
-              <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
-                <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
-                  <span className="text-sm font-bold">✓</span>
-                </span>{" "}
-                Automated task management workflow
-              </li>
-              <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
-                <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
-                  <span className="text-sm font-bold">✓</span>
-                </span>{" "}
-                Detailed analytics for your data
-              </li>
-              <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
-                <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
-                  <span className="text-sm font-bold">✓</span>
-                </span>{" "}
-                Some awesome integrations
-              </li>
-            </ul>
-          </div>
-          {/* <!-- End  Content --> */}
-
-          {/* <!-- Image --> */}
-          <div className="box-border relative w-full max-w-md px-4 mt-10 mb-4 text-center bg-no-repeat bg-contain border-solid md:mt-0 md:max-w-none lg:mb-0 md:w-1/2">
-            <Image
-              src="https://cdn.devdojo.com/images/december2020/settings.png"
-              className="pl-4 sm:pr-10 xl:pl-10 lg:pr-32"
-            />
-          </div>
-        </div>
-      </section>
-
-      {/* <!-- Section 5 --> */}
-      <section className="flex items-center justify-center py-20 bg-white min-w-screen">
-        <div className="px-16 bg-white">
-          <div className="container flex flex-col items-start mx-auto lg:items-center">
-            <p className="relative flex items-start justify-start w-full text-lg font-bold tracking-wider text-purple-500 uppercase lg:justify-center lg:items-center">
-              Don't just take our word for it
-            </p>
-
-            <h2 className="relative flex items-start justify-start w-full max-w-3xl text-5xl font-bold lg:justify-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                className="absolute right-0 hidden w-12 h-12 -mt-2 -mr-16 text-gray-200 lg:inline-block"
-                viewBox="0 0 975.036 975.036"
-              >
-                <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
-              </svg>
-              See what others are saying
-            </h2>
-            <div className="block w-full h-0.5 max-w-lg mt-6 bg-purple-100 rounded-full"></div>
-
-            <div className="items-center justify-center w-full mt-12 mb-4 lg:flex">
-              <div className="flex flex-col items-start justify-start w-full h-auto mb-12 lg:w-1/3 lg:mb-0">
-                <div className="flex items-center justify-center">
-                  <div className="w-16 h-16 mr-4 overflow-hidden bg-gray-200 rounded-full">
-                    <Image
-                      src="/chandan.jpeg"
-                      className="object-cover w-full h-full"
-                    />
-                  </div>
-                  <div className="flex flex-col items-start justify-center">
-                    <h4 className="font-bold text-gray-800">
-                      Chandan Kumar Maurya
-                    </h4>
-                    <p className="text-gray-600">CEO</p>
-                  </div>
-                </div>
-                <blockquote className="mt-8 text-lg text-gray-500">
-                  "This is a no-brainer if you want to take your business to the
-                  next level. If you are looking for the ultimate toolset, this
-                  is it!"
-                </blockquote>
-              </div>
-              <div className="flex flex-col items-start justify-start w-full h-auto px-0 mx-0 mb-12 border-l border-r border-transparent lg:w-1/3 lg:mb-0 lg:px-8 lg:mx-8 lg:border-gray-200">
-                <div className="flex items-center justify-center">
-                  <div className="w-16 h-16 mr-4 overflow-hidden bg-gray-200 rounded-full">
-                    <Image
-                      src="https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2547&amp;q=80"
-                      className="object-cover w-full h-full"
-                    />
-                  </div>
-                  <div className="flex flex-col items-start justify-center">
-                    <h4 className="font-bold text-gray-800">Jane Doe</h4>
-                    <p className="text-gray-600">CTO of Business</p>
-                  </div>
-                </div>
-                <blockquote className="mt-8 text-lg text-gray-500">
-                  "Thanks for creating this service. My life is so much easier.
-                  Thanks for making such a great product."
-                </blockquote>
-              </div>
-              <div className="flex flex-col items-start justify-start w-full h-auto lg:w-1/3">
-                <div className="flex items-center justify-center">
-                  <div className="w-16 h-16 mr-4 overflow-hidden bg-gray-200 rounded-full">
-                    <Image
-                      src="https://images.unsplash.com/photo-1545167622-3a6ac756afa4?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1256&amp;q=80"
-                      className="object-cover w-full h-full"
-                    />
-                  </div>
-                  <div className="flex flex-col items-start justify-center">
-                    <h4 className="font-bold text-gray-800">John Smith</h4>
-                    <p className="text-gray-600">Creator of Stuff</p>
-                  </div>
-                </div>
-                <blockquote className="mt-8 text-lg text-gray-500">
-                  "Packed with awesome content and exactly what I was looking
-                  for. I would highly recommend this to anyone."
-                </blockquote>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Productivity/>
+      <Leader/>
+      <Team/>
       <Accordion className="p-16">
         <AccordionItem
           key="1"
@@ -199,6 +35,7 @@ export default function Home() {
           {defaultContent}
         </AccordionItem>
       </Accordion>
+      
     </div>
   );
 }
